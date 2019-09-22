236a4692a5f0388d00adfe2dcdd71d27ff14cb17
Jaimerson Araújo <jaimersonaraujo@gmail.com>
2019-09-22 13:56:39 -0300

    First commit

---
+++ app/src/main/java/com/example/notetaker/CreateNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.notetaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
