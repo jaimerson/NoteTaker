ab2f212b425dfcd6a1c8564517e563911cbe4993
James <jaimersonaraujo@gmail.com>
2019-09-23 01:56:41 -0300

    Add note title dialog

--- app/src/main/java/com/example/notetaker/CreateNoteActivity.kt
+++ app/src/main/java/com/example/notetaker/CreateNoteActivity.kt
@@ -3,7 +3,8 @@ package com.example.notetaker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_create_note.*
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.note_form.*
 
 class CreateNoteActivity : AppCompatActivity() {
@@ -17,9 +18,24 @@ class CreateNoteActivity : AppCompatActivity() {
         }
 
         btn_confirm_add_note.setOnClickListener {
-            val note = Note("Some note", note_body.text.toString())
-            NoteRepository.addNote(note, this)
-            goToMainActivity()
+            val builder = AlertDialog.Builder(this)
+            val input = EditText(this)
+            builder.setTitle("Create note")
+            builder.setPositiveButton("OK") { dialog, _ ->
+                val title = input.text.toString()
+                if (title.isNotEmpty()){
+                    val note = Note(title, note_body.text.toString())
+                    NoteRepository.addNote(note, this)
+                    goToMainActivity()
+                } else {
+                    dialog.cancel()
+                }
+            }
+            builder.setNegativeButton("Cancel") { dialog, _ ->
+               dialog.cancel()
+            }
+            builder.setView(input)
+            builder.create().show()
         }
     }
 
